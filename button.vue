2ec132f4ebb970f4a90df8b784e8f0650faa1d29
anissa0511 <dingziling@shunwang.com>
2019-07-30 17:14:10 +0800

    zhihu

---
+++ button.vue
@@ -0,0 +1,47 @@
+<template>
+  <button @click="handleClick" :style="styles">
+    <slot></slot>
+  </button>
+</template>
+<script>
+export default {
+  props: {
+    color: {
+      type: String,
+      default: "#00cc66"
+    }
+  },
+  data() {
+    return {
+      myColor: this.color,
+      change: false
+    };
+  },
+  computed: {
+    styles() {
+      return {
+        background: this.myColor
+      };
+    }
+  },
+  methods: {
+    handleClick(e) {
+      this.myColor = "red";
+      this.$emit("click", e);
+    }
+  }
+};
+</script>
+<style scoped>
+button {
+  border: 0;
+  outline: none;
+  color: #fff;
+  padding: 4px 8px;
+}
+button:active {
+  position: relative;
+  top: 1px;
+  left: 1px;
+}
+</style>
